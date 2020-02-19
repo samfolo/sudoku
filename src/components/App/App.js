ff26187f40180140f074a0b91d4db4a55ca0ad28
Sam Folorunsho <s.folorunsho@hotmail.co.uk>
2020-02-19 13:47:48 +0000

    setup: create <App />

---
+++ src/components/App/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Classes from './App.module.css';
+
+function App() {
+  return (
+    <div data-test="component-app" className={Classes.App}>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/App/App.test.js
@@ -0,0 +1,18 @@
+import App from './App';
+import { setup, findByTestAttr } from '../../testHelpers';
+
+describe('<App />', () => {
+  let wrapper;
+  let appComponent;
+
+  beforeEach(() => {
+    wrapper = setup(App);
+    appComponent = findByTestAttr(wrapper, 'component-app');
+  });
+
+  it('renders without error', () => {
+    
+    expect(appComponent).toHaveLength(1);
+  });
+});
+

12f729628c98be7e16764c86e9d3c1ebad526530
Sam Folorunsho <s.folorunsho@hotmail.co.uk>
2020-02-21 12:02:49 +0000

    feat: <NumberButton /> diusplays its value

--- src/components/NumberSelector/NumberButton/NumberButton.js
+++ src/components/NumberSelector/NumberButton/NumberButton.js
@@ -3,7 +3,9 @@ import Classes from './NumberButton.module.css';
 
 const NumberButton = props => {
   return (
-    <div data-test="component-number-button" />
+    <div data-test="component-number-button">
+      {props.value}
+    </div>
   )
 }
 
--- src/components/NumberSelector/NumberButton/NumberButton.test.js
+++ src/components/NumberSelector/NumberButton/NumberButton.test.js
@@ -1,4 +1,4 @@
-import Number from './NumberButton';
+import NumberButton from './NumberButton';
 import { setup, findByTestAttr } from '../../../testHelpers';
 
 describe('<Number />', () => {
@@ -6,11 +6,16 @@ describe('<Number />', () => {
   let numberComponent;
 
   beforeEach(() => {
-    wrapper = setup(Number);
+    wrapper = setup(NumberButton);
     numberComponent = findByTestAttr(wrapper, 'component-number-button');
   });
 
   it('renders without error', () => {
     expect(numberComponent).toHaveLength(1);
+  });
+
+  it('displays its value', () => {
+    wrapper = setup(NumberButton, { value: 6 });
+    expect(wrapper.text()).toBe('6');
   });
 });
